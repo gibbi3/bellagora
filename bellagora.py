48a9ec223616f6fb510785a4d1e49595783b1330
Gil Birdsong <gibbirdsong@gmail.com>
2016-12-22 14:30:06 -0600

    changed function at bottom of bellagory.py

--- bellagora.py
+++ bellagora.py
@@ -413,5 +413,4 @@ def disconnect():
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    app.debug = True
-    app.run(host = '127.0.0.1', port = 5000)
+    app.run(debug = True)
