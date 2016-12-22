9e52b4710927b2939c8bbba7cdedc7b55f3a7a4c
Gil Birdsong <gibbirdsong@gmail.com>
2016-12-22 14:32:07 -0600

    altered import statements

--- bellagora.py
+++ bellagora.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_heroku import Heroku
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.heroku import Heroku
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
