4afcfdb4dcbbc779fb3715183f3c4da8cb58670c
Gil Birdsong <gibbirdsong@gmail.com>
2016-12-22 15:45:13 -0600

    redid requirements.txt

--- bellagora.py
+++ bellagora.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku
 
 from sqlalchemy import create_engine
@@ -16,6 +17,7 @@ import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gibbi3:algernon7@localhost/stock'
+db = SQLAlchemy(app)
 heroku = Heroku(app)
 
 CLIENT_ID = json.loads(
