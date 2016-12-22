6a730fe1b3813be0e5d46840943bb2d3362c387e
Gil Birdsong <gibbirdsong@gmail.com>
2016-12-22 17:15:04 -0600

    changed stuff

--- app.py
+++ app.py
@@ -414,4 +414,4 @@ if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
-    app.run(host = '127.0.0.1', port=port)
+    app.run(host = '0.0.0.0', port=port)
