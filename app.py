42a089ad21ef4c84f45126a7dc289777dc34499f
Gil Birdsong <gibbirdsong@gmail.com>
2016-12-27 19:37:05 -0600

    alter path for JSON

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ def categoryStockJSON(category_id):
     return jsonify(Items=[i.serialize for i in items])
 
 
-@app.route('/bellagora/<int:category_id>/item<int:item_id>/JSON/')
+@app.route('/bellagora/<int:category_id>/<int:item_id>/JSON/')
 def itemJSON(category_id, item_id):
     item = session.query(Item).filter_by(id=item_id).one()
     return jsonify(Item=item.serialize)
